2fc4666874fe4ca4f869eaf9b45427a0fc49ff24
Sarai Rojas <sara.r.sandoval@gmail.com>
2022-02-03 17:25:33 -0600

    Add filterData method to order object in main.js

--- src/data.js
+++ src/data.js
@@ -1,9 +1,30 @@
-// estas funciones son de ejemplo
+const order = {
+  filterData: (data, filter) => {
+    let infoArray = [];
 
-export const example = () => {
-  return 'example';
+    if (filter === 'title') {
+      for (let i = 0; i < data.films.length; i++) {
+        infoArray.push(data.films[i][filter]);
+      }
+    } else if (filter === 'people' || filter === 'vehicles' || filter === 'locations') {
+      for (let i = 0; i < data.films.length; i++) {
+        for (let j = 0; j < data.films[i][filter].length; j++) {
+          infoArray.push(data.films[i][filter][j].name);
+        }
+      }
+    } else if (filter === 'staff') {
+      for (let i = 0; i < data.films.length; i++) {
+        if (!infoArray.includes(data.films[i].director)) {
+          infoArray.push(data.films[i].director);
+        }
+        if (!infoArray.includes(data.films[i].producer)) {
+          infoArray.push(data.films[i].producer);
+        }
+      }
+    }
+
+    return infoArray;
+  },
 };
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+export default order;
