6faec54dbceff96cf38360ff145763d2fbab85dd
skarlet <skarlet15m@gmail.com>
2022-02-03 20:58:56 -0600

    Mostarr u obtener todos los pokemones

--- src/main.js
+++ src/main.js
@@ -1,46 +1,71 @@
-import { example } from './data.js';
+//import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+//console.log(example, data);
+//contenedor principal
+const principalContainer= document.querySelector(".cards-containers-principal");
 
-const namePokemon=document.querySelector(".name-pokemon");
-const titleName=document.querySelector(".title-name");
-const idPokemon=document.querySelector(".id-pokemon");
-const titleId=document.querySelector(".title-id");
-const classImagenPokemon=document.querySelector(".imagen-pokemon");
-const imagenPokemon=document.createElement("p");
-const typePokemon = document.querySelector(".type-pokemon");
-const titleType=document.querySelector(".title-type");
+//constante de longitud
 const lengthData=data.pokemon.length;
-console.log(lengthData);
+
 
 function pokemon(){
+
     
-    for(let i=0; i<lengthData-1; i++){
-        console.log(data.pokemon[i].type)
-    }
-    const imagen=data.pokemon[0].img;
-    imagenPokemon.innerHTML= `<img src="${imagen}" alt="">`;
-    classImagenPokemon.parentNode.appendChild(imagenPokemon);
+    for(let i=0; i<lengthData; i++){
 
+        //contenedor white
+        let containerExternalCard=document.createElement("div");
+        containerExternalCard.classList.add("card-container");
+        //Crear div con una clase para la imagen
+        let classImagenPokemon=document.createElement("div");
+        let imagenPokemon=document.createElement("img");
+        classImagenPokemon.classList.add("imagen-pokemon");
 
-    const nombre= data.pokemon[0].name;
-    titleName.innerText = "Nombre: "
-    namePokemon.innerText=nombre;
+        //contenedor black
+        let containerInternalCard=document.createElement("div");
+        containerInternalCard.classList.add("caracter-pokemon");
+
+        //crear div con una clase para el nombre
+        let namePokemon=document.createElement("div");
+        namePokemon.classList.add("name-pokemon");
+
+        //crear div con una clase para el identificador
+        let idPokemon=document.createElement("div");
+        idPokemon.classList.add("id-pokemon");
+
+        //crear div con una clase para los tipos
+        let typePokemon = document.createElement("div");
+        typePokemon.classList.add("type-pokemon");  
         
+        let imagen=data.pokemon[i].img;
+        imagenPokemon.src= imagen;
+        classImagenPokemon.appendChild(imagenPokemon);
 
-    const identificador= data.pokemon[0].num;
-    titleId.innerText = "ID: "
-    idPokemon.innerText=identificador;
 
-    const tipos= data.pokemon[0].type;
-    titleType.innerText = "Tipo: "
-    typePokemon.innerText=tipos;
+        let nombre= data.pokemon[i].name;
+        namePokemon.innerText=`Nombre: ${nombre}`;
+            
+
+        let identificador= data.pokemon[i].num;
+        idPokemon.innerText=`Id: ${identificador}`;
+
+        let tipos= data.pokemon[i].type;
+        typePokemon.innerText=`Tipos: ${tipos}`;
+
+        containerExternalCard.appendChild(classImagenPokemon);
+        containerInternalCard.appendChild(namePokemon);
+        containerInternalCard.appendChild(idPokemon);
+        containerInternalCard.appendChild(typePokemon);
+
+        containerExternalCard.appendChild(containerInternalCard);
+
+        principalContainer.appendChild(containerExternalCard);
+        
     }
-    
-    
+}
 
 
 pokemon();
