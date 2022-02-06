e24c48d8a9ae79a969957be7c87c5337a611cc47
Sarai Rojas <sara.r.sandoval@gmail.com>
2022-02-05 21:44:50 -0600

    Change filterData loops to forEach and keep info as a list of objects

--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
 const order = {
   filterData: (data, filter) => {
+    /*
     let infoArray = [];
 
     if (filter === 'title') {
@@ -12,18 +13,32 @@ const order = {
           infoArray.push(data.films[i][filter][j].name);
         }
       }
-    } else if (filter === 'staff') {
-      for (let i = 0; i < data.films.length; i++) {
-        if (!infoArray.includes(data.films[i].director)) {
-          infoArray.push(data.films[i].director);
-        }
-        if (!infoArray.includes(data.films[i].producer)) {
-          infoArray.push(data.films[i].producer);
-        }
-      }
     }
-
     return infoArray;
+    */
+    let info = [];
+
+    if (filter === 'title') {
+      data.films.forEach((item) => {
+        const infoFilter = {};
+        infoFilter.poster = item.poster;
+        infoFilter.title = item.title;
+        infoFilter.release_date = item.release_date;
+        infoFilter.rt_score = item.rt_score;
+        info.push(infoFilter);
+      });
+    } else if (filter === 'people' || filter === 'vehicles' || filter === 'locations') {
+      data.films.forEach((item) => {
+        for (let j = 0; j < item[filter].length; j++) {
+          const infoFilter = {};
+          infoFilter.img = item[filter][j].img;
+          infoFilter.name = item[filter][j].name;
+          infoFilter.title = item.title;
+          info.push(infoFilter);
+        }
+      });
+    }
+    return info;
   },
 };
 
