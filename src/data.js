b15e46d348ce8682b57ccf605fef9bdf0d64ad3a
Sarai Rojas <sara.r.sandoval@gmail.com>
2022-02-07 11:24:23 -0600

    Delate unused code in filterData function

--- src/data.js
+++ src/data.js
@@ -1,21 +1,7 @@
 const order = {
-  filterData: (data, filter) => {
-    /*
-    let infoArray = [];
 
-    if (filter === 'title') {
-      for (let i = 0; i < data.films.length; i++) {
-        infoArray.push(data.films[i][filter]);
-      }
-    } else if (filter === 'people' || filter === 'vehicles' || filter === 'locations') {
-      for (let i = 0; i < data.films.length; i++) {
-        for (let j = 0; j < data.films[i][filter].length; j++) {
-          infoArray.push(data.films[i][filter][j].name);
-        }
-      }
-    }
-    return infoArray;
-    */
+  filterData: (data, filter) => {
+    
     let info = [];
 
     if (filter === 'title') {
