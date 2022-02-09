b3511a04af869669d32188ee4de389dc2a71eabd
Atenas Villalba <atenasvillalba8@gmail.com>
2022-02-08 18:42:43 -0600

    Cambios en codigo para la manipulación de la data

--- src/data.js
+++ src/data.js
@@ -1,6 +1,19 @@
+import data from "./data/pokemon/pokemon.js";
 // estas funciones son de ejemplo
 
-export const example = (data,condition) => {
+console.log(data);
+
+let dataPokemons = data.pokemon;
+console.log(dataPokemons);
+
+let eachPokemon = Object.keys(dataPokemons);
+for (let i = 0; i < dataPokemons.length; i++) {
+  let ePokemon = eachPokemon[i];
+  console.log(dataPokemons[ePokemon]);
+  console.log(ePokemon);
+}
+
+export const example = (data, condition) => {
   return "example";
 };
 
