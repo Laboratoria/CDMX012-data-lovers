0878ac7487e415ca8f5717aad369b370b48a1436
Daniela Miñon <danielamactriz@gmail.com>
2022-02-09 10:03:06 -0600

    primer avance pokemon

--- src/data.js
+++ src/data.js
@@ -1,9 +1,82 @@
-// estas funciones son de ejemplo
 
-export const example = () => {
-  return 'example';
-};
+const dataPokemon={
 
-export const anotherExample = () => {
-  return 'OMG';
+
+
+  pokemonResistant : (data, filterResistant)=>{
+let arrayResistant=[];
+let pokemon= data.pokemon;
+ 
+  for(let i=0; i<pokemon.length; i++){
+    let resistant=pokemon[i].resistant;
+  
+    for(let j=0; j<resistant.length; j++){
+     if(resistant[j]== filterResistant)
+     arrayResistant.push(pokemon[i]);
+    }
+  
+ 
+  return arrayResistant;
+  }
+},
+
+byName: (data)=>{
+  let pokemon=data.pokemon.slice(0);
+  let byName=[];
+  byName= pokemon.sort((a,b)=>{
+  if(a.name>b.name){
+ return 1   
+  }
+  return-1
+})
+return byName
+},
+
+
+/*pokemonName:(data)=>{
+let pokemon= data.pokemon;
+let arrayPokemones= [];
+  for(let i=0; i<pokemon.length; i++){
+    arrayPokemones.push(pokemon[i].name);
+    arrayPokemones.sort();
+    
+  }
+
+  return arrayPokemones;
+
+},*/
+ pokemonNumber:(data)=>{
+  let arrayNumbers= data.pokemon;
+  //let arrayNumbers=[];
+  //for(let i=0; i<pokemon.length; i++){
+  // arrayNumbers.push(pokemon[i].num)
+  //}
+    return arrayNumbers;
+ },
+
+ tPokemon : (data) => {
+  let tPokemon= {};
+  let pokemon= data.pokemon;
+  
+ 
+  for(let i=0; i<pokemon.length; i++){
+    let types=pokemon[i].type;
+  
+    for(let j=0; j<types.length; j++){
+     let typePokemon= types[j]; 
+     
+     if(!Object.prototype.hasOwnProperty.call(tPokemon, typePokemon)){
+       tPokemon[typePokemon]=[];
+      
+     }
+    
+     tPokemon[typePokemon].push(pokemon[i]);
+    }
+  }
+  return tPokemon;
+
+},
+
+
 };
+export default dataPokemon;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,73 @@
-import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+
+import dataPokemon from './data.js';
+import data from './data/pokemon/pokemon.js';
+
+document.getElementById("numerico").addEventListener("click", showNumbers );
+document.getElementById("nombre").addEventListener("click", showName);
+//document.getElementById("tipo").addEventListener("click", showType);
+document.getElementById("tipo").addEventListener("click", showTypeBox);
+document.getElementById("fireType").addEventListener("click", htmlStructure);
+
+
+function showNumbers(){
+  document.getElementById("number").innerHTML = arrayNumbers[0].num;
+  document.getElementById("img").src = arrayNumbers[0].img;
+  document.getElementById("name").innerHTML = arrayNumbers[0].name;
+  
+  
+
+}
+function showName(){
+  document.getElementById("number").innerHTML = byName[0].num;
+  document.getElementById("name").innerHTML = byName[0].name;
+  document.getElementById("img").src = byName[0].img;
+  
+ 
+  }
+  //function showType(){
+   // document.getElementById("typeBox").innerHTML=Object.getOwnPropertyNames(type).sort();;
+  
+    
+ // }
+  function showTypeBox(){
+    document.getElementById("type").style.display="block"
+    //document.getElementById("fireType").addEventListener("click", showFire )
+    
+    /*function showFire(){
+      document.getElementById("number").innerHTML =  type.fire[1].num;
+      document.getElementById("name").innerHTML = type.fire[1].name;
+      document.getElementById("img").src = type.fire[1].img;
+      
+    }*/
+  }
+
+  function htmlStructure(){
+let div= document.getElementById("infoBox")
+document.getElementById("allPokemons").style.display="block";
+
+
+for(let i=0; i<type.fire.length; i++){
+  
+  let resultado= "";
+  resultado+='<div class= "flexBox">' 
+  resultado+="<p>"+ type.fire[i].num + "</p>"
+  resultado+= "<img src='"+ type.fire[i].img+"'></img>"
+  resultado+= "<p>" + type.fire[i].name + "</p>" 
+  resultado+="</div>"
+  div.innerHTML+=resultado;
+  
+
+}
+
+ 
+
+}
+
+
+  const type= dataPokemon.tPokemon(data);
+  const byName= dataPokemon.byName(data);
+ const arrayNumbers= dataPokemon.pokemonNumber(data);
+ 
+
+ 
