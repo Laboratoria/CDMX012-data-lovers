f67863b71cf51eaa1510cf105530b9dd99855e34
Lorenarojas1 <lorena.rojas080912@gmail.com>
2022-02-09 00:29:27 -0600

    Funcion ordenar

--- src/data.js
+++ src/data.js
@@ -1,13 +1,21 @@
-import data from './data/pokemon/pokemon.js';
 
+export const getArray = datos => (Array.isArray(datos) ? datos: Object.values(datos));
 // estas funciones son de ejemplo
-export const filterData = (data, condition) => {
-return
+export const filterDataName = (datos, condition) => getArray(datos).filter(({name}) => name.toLowerCase().startsWith(condition));
+
+//ascendete y  descendente
+
+export const orderAscendente = (datos) => getArray(datos).sort((a, b ) => (a.num < b.num  ? -1 : 1));
+
+export const orderDescendente = (datos) => getArray(datos).sort((a, b ) => (a.num > b.num  ? -1 : 1));
+
+//a-z
+export const orderA_Z = (datos) => getArray(datos).sort((a, b ) => (a.name < b.name  ? -1 : 1));
+
+//z-a
+export const orderZ_A = (datos) => getArray(datos).sort((a, b ) => (a.name > b.name  ? -1 : 1));
 
 
-
-  return filterData;
-};
 
 export const anotherExample = () => {
   return 'OMG';
