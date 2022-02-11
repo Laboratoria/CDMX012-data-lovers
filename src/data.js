01bef6db83b02764f773d6f21b84db760d6b6930
Atenas Villalba <atenasvillalba8@gmail.com>
2022-02-11 09:56:42 -0600

    Creando ventana modal

--- src/data.js
+++ src/data.js
@@ -3,6 +3,7 @@ import pokemon from "./data/pokemon/pokemon.js";
 import data from "./data/pokemon/pokemon.js";
 // estas funciones son de ejemplo
 
+
 export const orderByAz = () => {
   return "example";
 };
@@ -12,15 +13,12 @@ export const orderByZa = () => {
 };
 
 export const filterByTypes = () => {
- 
   return "OMG";
 };
 
 export const filterByEvolution = () => {
- 
   return "OMG";
 };
 export const filterByLegendary = () => {
- 
   return "OMG";
 };
--- src/main.js
+++ src/main.js
@@ -25,21 +25,32 @@ for (let property of data.pokemon) {
   let identificador = "pokeCards"; //nombre de la clase
   let idDirection = "#modalWindowPokemon";
   let idSeccion = property.num;
+  let modalDataOpen = "idModal"; //**********tal vez pueda ir en la seccion o en el num, ya que num es el id de cada iteracion
+
   const modalWindow = document.createElement("a");
   modalWindow.setAttribute("href", idDirection);
+
   const cardSpace = document.createElement("section"); //crea una sección
   cardSpace.setAttribute("class", identificador); //crea el atributo clase identificador='pokeCards'
   cardSpace.setAttribute("id", idSeccion);
+
   const cardName = document.createElement("p"); //crea el elemento parrafo
-  cardName.innerHTML = "Pokémon: <br>" + `${property.name}`; // cambia el texto vacio del parrafo por la propiedad name
+  cardName.innerHTML =
+    "Pokémon: <br>" +
+    `${property.name[0].toUpperCase() + property.name.slice(1)}`; // cambia el texto vacio del parrafo por la propiedad name convirtiendo la primera letra del array en mayuscula y concatenando desde la posicion 1 con slice
+
   const cardNum = document.createElement("p"); //crea el elemento parrafo
   cardNum.innerHTML = property.num; // cambia el texto vacio del parrafo por la propiedad numero
+
   const pokemonImag = document.createElement("img"); //crea un Elemento de tipó imagen
   pokemonImag.setAttribute("src", `${property.img}`); //le asigna el atributo de src con su propiedad que es el link
+  pokemonImag.setAttribute("data-open", modalDataOpen);
+
   cardSpace.appendChild(pokemonImag); //cardSpace es el nodo padre y apokemonImag es el nodo hijo
   cardSpace.appendChild(cardName); //cardSpace es el nodo padre y cardName es el nodo hijo
   cardSpace.appendChild(cardNum); //cardSpace es el nodo padre y cardNumes el nodo hijo
   modalWindow.appendChild(cardSpace);
+
   document.getElementById("showData").appendChild(modalWindow); //show data es el nodo padre y cardSpace el nodo hijo.
   showTotalPokemons = cardSpace;
 }
@@ -48,25 +59,66 @@ for (let property of data.pokemon) {
 
 for (let content of data.pokemon) {
   let showTotalContent = [""];
-  let idContent = content.num;
+  let idModalContent = content.num;
+  let classModalDialog = "modalDialog";
+  let closeModal = "closeModal";
+  let modalTitle = "modalTitle";
   let aboutPokemon = content.about;
   let resistantPokemon = content.resistant;
   let weaknessesPokemon = content.weaknesses;
   let typePokemon = content.type;
 
   const allContent = document.createElement("section");
-  allContent.setAttribute("id", idContent);
+  allContent.setAttribute("id", idModalContent);
+  allContent.setAttribute("class", classModalDialog);
+
+  const buttonModal = document.createElement("input");
+  buttonModal.setAttribute("class", closeModal);
+  buttonModal.setAttribute("aria-label", "closeModal");
+  buttonModal.setAttribute("value", "X");
+  buttonModal.setAttribute("type", "button");
+
   const contentTitle = document.createElement("h2");
   contentTitle.innerHTML = content.name.toUpperCase();
+  contentTitle.setAttribute("class", modalTitle);
+
+  const contentImag = document.createElement("img"); //crea un Elemento de tipó imagen
+  contentImag.setAttribute("src", `${content.img}`); //le asigna el atributo de src con su propiedad que es el link
+  //pokemonImag.setAttribute("data-open", modalDataOpen);
+
   const contentAbout = document.createElement("p");
   contentAbout.innerHTML =
-    "Datos: <br>" + "Descripción" + aboutPokemon + "Tipo: " + typePokemon;
-  "Resistencia" + resistantPokemon + "Debilidades" + weaknessesPokemon;
+    "Datos: <br>" +
+    "Descripción <br>" +
+    aboutPokemon +
+    "<br>Tipo: <br>" +
+    typePokemon +
+    "<br>Resistencia: <br>" +
+    resistantPokemon +
+    "<br>Debilidades:<br> " +
+    weaknessesPokemon;
+
+  allContent.appendChild(buttonModal);
   allContent.appendChild(contentTitle);
+  allContent.appendChild(contentImag);
   allContent.appendChild(contentAbout);
-  document.getElementById("popUpWindow").appendChild(allContent);
+  document.getElementById("idModal").appendChild(allContent);
   showTotalContent = allContent;
 }
+/*
+const openModal = document.querySelector("data-open]");
+const isVisibility = "is-visibility";
+const closeModal = document.querySelector("closeModal");
+
+for (const modal of openModal) {
+  modal.addEventListener("click", function () {
+    const modalId = this.dataset.open;
+    document.getElementById("idModal").classList.add(isVisibility);
+  });
+}*/
+/*
+for(const closeModal of openModal)
+*/
 /*
 HACER UN CONSOLE.LOG DE LA DATA            S
 ACCDER A LOS 3 ELEMENTOS JUNTOS EN UNA MISMA FILA 
