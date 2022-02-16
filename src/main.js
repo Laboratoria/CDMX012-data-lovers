89bcbc14659e364b31fe8eeb0e5f754400006d70
roxanaggr <roxanaggr@gmail.com>
2022-02-16 10:15:31 -0600

    la imagen y el texto ya estan juntos en un bloque

--- src/main.js
+++ src/main.js
@@ -3,16 +3,16 @@ import data from './lol/lol.js';
 
 let arrayChampions = Object.values(data.data)
     for (let champion of arrayChampions) {
-    let img = document.createElement('img')
-    img.src = champion.splash
-    let img2 = document.getElementById('image')
-        img2.appendChild(img)
-    
-    let name = document.createElement('h1')
-        name.textContent = champion.name
-    let name2 =document.getElementById('champion')
-        name2.appendChild(name)
 
-    console.log(name2)
-    
+    let img = document.createElement('img')
+        img.src = champion.splash
+    let name = document.createElement('h5')
+        name.textContent = champion.name
+
+    let cardChampion = document.getElementById('root')
+    cardChampion.appendChild(name).appendChild(img)
+
+    //console.log(championInfo)
 }
+
+/**/
