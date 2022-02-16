2b57dde7b6981e1b24d7713f830fb72353f581a0
Lorenarojas1 <lorena.rojas080912@gmail.com>
2022-02-15 22:23:56 -0600

    Responsive design y pruebas unitarias

--- src/data.js
+++ src/data.js
@@ -1,31 +1,29 @@
-
-// estas funciones son de ejemplo
-
+// FUNCIÓN DE FILTRADO POR TIPO DE POKEMON
 export const filterDataByType = (datos, tipo) =>{
   return datos.filter(dato => dato.type.includes(tipo));
 };
 
+// FUNCIÓN DE FILTRADO POR NOMBRE DE POKEMON
 export const filterDataByName = (datos, name) =>{
   return datos.filter(dato => dato.name == name);
-  
 };
 
+// FUNCIÓN DE FILTRADO POR ID DE POKEMON
 export const filterDataById = (datos, id) =>{
   return datos.filter(dato => dato.num == id);
-  
 };
 
-//ascendete y  descendente
-
+//FUNCIÓN DE ORDENAR DE ASCENDENTE
 export const orderAscendente = (datos) => datos.sort((a, b ) => (a.num < b.num  ? -1 : 1));
 
+//FUNCIÓN DE ORDENAR DE DESCENDENTE
 export const orderDescendente = (datos) => datos.sort((a, b ) => (a.num > b.num  ? -1 : 1));
 
-//a-z
-export const orderA_Z = (datos) => datos.sort((a, b ) => (a.name < b.name  ? -1 : 1));
+//FUNCIÓN DE ORDENAR DE ALFABETICAMENTE (A-Z)
+export const orderAZ = (datos) => datos.sort((a, b ) => (a.name < b.name  ? -1 : 1));
 
-//z-a
-export const orderZ_A = (datos) => datos.sort((a, b ) => (a.name > b.name  ? -1 : 1));
+//FUNCIÓN DE ORDENAR DE ALFABETICAMENTE (Z-A)
+export const orderZA = (datos) => datos.sort((a, b ) => (a.name > b.name  ? -1 : 1));
 
 
 
--- test/data.spec.js
+++ test/data.spec.js
@@ -1,102 +1,58 @@
-import { orderA_Z, orderZ_A, orderAscendente, orderDescendente, filterDataByType} from '../src/data.js';
+import { orderAZ, orderZA, orderAscendente, orderDescendente, filterDataByType, filterDataByName, filterDataById} from '../src/data.js';
 
 describe('funciones de ordenamiento', () => {
   it('ordena de la A a la Z', () => {
-    const dataExample = [
-      {
-        name:"picachu",
-      },
-      {
-        name:"raichu",
-      },
-      {
-        name:"nidorina",
-      },
-    ];
+    const dataExample = [{"name": "charmeleon"}, {"name": "squirtle"},{"name": "bulbasaur"}];
+    const dataResponse = [{"name": "bulbasaur"}, {"name": "charmeleon"}, {"name": "squirtle"}];
 
-    expect(orderA_Z(dataExample)).toBeDefined();
+    expect(orderAZ(dataExample)).toStrictEqual(dataResponse);
   });
 
   it('ordena de la Z a la A', () => {
-    const dataExample = [
-      {
-        name:"venusaur",
-      },
-      {
-        name:"wartortle",
-      },
-      {
-        name:"caterpie",
-      },
-    ];
+    const dataExample = [{"name":"venusaur"}, {"name":"wartortle"}, {"name":"caterpie"}];
+    const dataResponse = [{"name": "wartortle"}, {"name": "venusaur"}, {"name": "caterpie"}];
 
-    expect(orderZ_A(dataExample)).toBeDefined();
+    expect(orderZA(dataExample)).toStrictEqual(dataResponse);
+
   });
 
   it('ordena de menor a mayor', () => {
-    const dataExample = [
-      {
-        num:"035",
-      },
-      {
-        num:"150",
-      },
-      {
-        num:"002",
-      },
-    ];
+    const dataExample = [{"num":"035"},{"num":"150"},{"num":"002"}];
+    const dataResponse= [{"num":"002"},{"num":"035"},{"num":"150"}];
 
-    expect(orderAscendente(dataExample)).toBeDefined();
+    expect(orderAscendente(dataExample)).toStrictEqual(dataResponse);
   });
 
   it('ordena de mayor a menor', () => {
-    const dataExample = [
-      {
-        num:"055",
-      },
-      {
-        num:"251",
-      },
-      {
-        num:"080",
-      },
-    ];
+    const dataExample = [{"num":"085"},{"num":"159"},{"num":"022"}];
+    const dataResponse= [{"num":"159"},{"num":"085"},{"num":"022"}];
 
-    expect(orderDescendente(dataExample)).toBeDefined();
-  });
+    expect(orderDescendente(dataExample)).toStrictEqual(dataResponse);
 });
-
-
+});
 
 describe('funciones de filtrado', () => {
   it('filtrado por tipo de pokemon', () => {
     const dataExample = [
-      {
-        name:'pikachu',
-      },
-      {
-        name:'bulbasaur',
-      },
-      {
-        name:'furret',
-      },
-    ]
-    const types = [
-      {
-        type: 'electric',
-      },
-      {
-        type: 'poison',
-      },
-      {
-        type: 'normal',
-      },
-    ]
+      {"name":'pikachu',"type":["electric"]},
+      {"name":'bulbasaur',"type":["grass", "poison"]},
+      {"name":'furret',"type":["normal"]}];
+    const dataResponse =[{"name": "pikachu","type":["electric"]}];
 
-    expect(filterDataByType(dataExample, types)).toBe(types);
+    expect(filterDataByType(dataExample, "electric")).toStrictEqual(dataResponse);
   });
-  it('filtrado por tipo de pokemon', () => {
 
-    expect(filterDataByType('pikachu','electric' )).toBe('electric');
+  it('filtrado por nombre de pokemon', () => {
+    const dataExample = [{"name":'kharmander'},{"name":'Charmander'},{"name":'charmander'}];
+    const dataResponse =[{"name": "charmander"}];
+
+    expect(filterDataByName(dataExample, "charmander")).toStrictEqual(dataResponse);
+  });
+
+  it('filtrado id de pokemon', () => {
+    const dataExample = [{"num":'039'},{"num":'39'},{"num":'390'}];
+    const dataResponse =[{"num": "039"}];
+
+    expect(filterDataById(dataExample, "039")).toStrictEqual(dataResponse);
   });
 });
