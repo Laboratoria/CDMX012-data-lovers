2d0a59294512640083dd817077b811506d2ecaea
AnaLizarraga <analiz.laboratoria@gmail.com>
2022-02-18 11:13:59 -0600

    orden A-Z consola

--- src/main.js
+++ src/main.js
@@ -51,7 +51,69 @@ for (const pokemon of pokemonList) {
   }
 }
 
+document.getElementById("order").addEventListener('click', orderList , false); //informacion del boton order
 
-//console.log(pokemonList)
+let pokemonNames = new Array (); //array lista nombres
+let pokemonNum= new Array(); //array lista numero
+for (let pokemon of pokemonList) { //envia los pokemones al array
+  pokemonNames.push(pokemon.name);
+  pokemonNum.push(pokemon.num);
+}
 
-//console.log(example, data);
+function orderList(){ //funcion de ordenado
+  let orderType= document.getElementById("order").value; //toma el valor de la opcion seleccionada
+  //console.log(orderType);
+  switch (orderType){ // muestra la información segun el caso elegido
+    case "AZ":
+      console.log("ordena el array de la A-z");
+      orderAZ(); //la función que se ejecutará en caso de ser seleccionada
+    break;
+    case "ZA":
+      console.log("ordena el array de la Z-A");
+      orderZA();
+    break;
+    case "lowest":
+      console.log("ordena el array de LOWEST");
+      orderLowest();
+    break;
+    case "higher":
+      console.log("ordena el array de la higher");
+      orderHighest();
+    break;
+  }
+}
+function orderAZ(){ 
+  let pokemonNameAZ = pokemonNames.sort(); //ordena los pokenones de la A-Z
+  let pokemonAz = new Array ();
+
+  for (const name of pokemonNameAZ) {
+    for (const pokemon of pokemonList) {
+      if(pokemon.name === name){
+
+        pokemonAz.push(pokemon)
+      }
+    }
+  }
+  console.log(pokemonAz);
+}
+
+
+function orderZA(){  //ordena los pokenones de la Z-A
+  console.log(pokemonNames.sort().reverse());
+}
+
+function orderLowest(){
+  pokemonNum.sort(function(a, b){return a-b});  //Ordena los pokemon por número 001-251
+  console.log(pokemonNum.sort(function(a, b){return a-b}));
+}
+
+function orderHighest(){ //Ordena los pokemon por número 251-001
+  pokemonNum.sort(function(a, b){return b-a});
+  console.log(pokemonNum.sort(function(a, b){return b-a}));
+}
+
+// console.log(pokemonList[0])
+// for (const pokemon of pokemonList) {
+//   if(pokemon.name === "squirtle"){
+//     console.log(pokemon);
+//   }
