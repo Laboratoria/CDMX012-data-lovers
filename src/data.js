185d8ba010ae8e85ec1c584c3cecf5be6f9242a9
Allison Tovar <alis.th94@gmail.com>
2022-02-21 20:26:25 -0600

    Se dividieron las funciones del manejos del DOM y se redujeron las funciones.

--- src/data.js
+++ src/data.js
@@ -1,9 +1,97 @@
-// estas funciones son de ejemplo
+import data from "./data/pokemon/pokemon.js";
 
-export const example = () => {
-  return 'example';
-};
+let pokemonList = data.pokemon;
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+///////////////////////CONSTRUCCIÓN DE NUEVOS ARRAYS PARA MOSTRAR LA INFORMACIÓN
+
+function buildPokemonNames() {
+  let pokemonNames = new Array(); //array lista nombres
+
+  for (let pokemon of pokemonList) {
+    pokemonNames.push(pokemon.name); //envia los pokemones por nombre al array
+  }
+  return pokemonNames;
+}
+
+function buildPokemonNums() {
+  let pokemonNum = new Array(); //array lista nombres
+
+  for (let pokemon of pokemonList) {
+    pokemonNum.push(pokemon.num); //envia los pokemones por numero al array
+  }
+  return pokemonNum;
+}
+
+///////////////////////ORDENADO
+
+export const orderAZ = () => {
+  // Ordenado A-Z
+  let pokemonNameAZ = buildPokemonNames().sort(); //ordena los pokemones de la A-Z
+  let pokemonAz = new Array(); // guarda la ejecucion de la funcion
+
+  for (const name of pokemonNameAZ) {
+    // busca en la lista de nombres ordenados por nombre
+    for (const pokemon of pokemonList) {
+      // busca el arreglo de objetos del pokemon en la lista de todos los pokemones
+      if (pokemon.name === name) {
+        // compara los atributos por nombres
+        pokemonAz.push(pokemon); //envia los pokemones por numero al array
+      }
+    }
+  }
+  return pokemonAz;
+}
+
+export const orderLowest = () => {
+  //Ordena los pokemon por número MENOR
+  let pokemonNumLowest = buildPokemonNums().sort(function (a, b) {
+    return a - b;
+  }); //Ordena los pokemon por número 001-251 comparando el valor de su atributo num
+  let pokemonLowest = new Array(); // guarda la ejecucion de la funcion
+
+  for (const num of pokemonNumLowest) {
+    // busca por el numero del pokemon en la lista de pokemones ordenados
+    for (const pokemon of pokemonList) {
+      // busca el arreglo de objetos del pokemon en la lista de todos los pokemones
+      if (pokemon.num === num) {
+        // compara el pokemon por su numero
+        pokemonLowest.push(pokemon); // envia el pokemon por nombre ordenado a la lista de pokemones LOWEST
+      }
+    }
+  }
+  return pokemonLowest;
+}
+
+
+///////////////////////FILTRADO
+
+export const fillterType = () => {
+  let FilTypeSelected = document.getElementById("filter").value; //toma el valor de la opcion seleccionada HTML id=filter
+  let FilType = new Array(); // guarda la ejecucion del for
+
+  for (const pokemon of pokemonList) {
+    // busca plos pokemones en la lista de pokemones
+    if (
+      pokemon.type[0] === FilTypeSelected ||
+      pokemon.type[1] === FilTypeSelected
+    ) {
+      // busca el valor en la posición 1 o 2 de los tipos
+      FilType.push(pokemon); // envia el pokemon filtrado al array
+    }
+  }
+  return FilType;
+}
+
+/////////////////////// BUSQUEDA
+
+export const inputPokemon = () => {
+  let inputPokemonSearch = document.getElementById("inputSearch").value.toLowerCase();
+  let pokemonSelected = new Array();
+
+  for (const pokemon of pokemonList) {
+    if (pokemon.name.includes(inputPokemonSearch)) {
+      pokemonSelected.push(pokemon);
+    }
+  }
+  return pokemonSelected;
+}
--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
-import data from "./data/pokemon/pokemon.js";
-// import data from './data/pokemon/pokemon.js';
+import { orderAZ, orderLowest, fillterType, inputPokemon } from "./data.js";
 
-let pokemonList = data.pokemon;
+//let pokemonList = data.pokemon;
 let pokemonContainer = document.getElementById("root");
 
 function buildCards(pokemonArray) {
   // crea las cards de pokemones
 
-  console.log(pokemonArray);
-
   for (const pokemon of pokemonArray) {
-    let card = document.createElement("pokemon");
+    let card = document.createElement("section");
     card.className = "card";
 
     let pokemonNum = document.createElement("div");
@@ -26,21 +23,15 @@ function buildCards(pokemonArray) {
     let barrita = document.createElement("div");
     barrita.className = "barrita";
 
-    //let pokemonType = document.createElement("div");
-    //pokemonType.className = "property type";
-
     let num = document.createTextNode(`${pokemon.num}`);
     let name = document.createTextNode(`${pokemon.name}`);
-    //let type = document.createTextNode(`${pokemon.type}`);
 
     pokemonNum.appendChild(num);
     pokemonName.appendChild(name);
-    // pokemonType.appendChild(type);
     card.appendChild(pokemonNum);
     card.appendChild(pokemonImg);
     card.appendChild(pokemonName);
     card.appendChild(barrita);
-    //card.appendChild(pokemonType);
     pokemonContainer.appendChild(card);
 
     for (let type of pokemon.type) {
@@ -52,26 +43,6 @@ function buildCards(pokemonArray) {
       card.appendChild(divType); //agrega los divs a la card
     }
   }
-}
-
-/////////////////////////ORDENADO
-
-function buildPokemonNames() {
-  let pokemonNames = new Array(); //array lista nombres
-
-  for (let pokemon of pokemonList) {
-    pokemonNames.push(pokemon.name); //envia los pokemones por nombre al array
-  }
-  return pokemonNames;
-}
-
-function buildPokemonNums() {
-  let pokemonNum = new Array(); //array lista nombres
-
-  for (let pokemon of pokemonList) {
-    pokemonNum.push(pokemon.num); //envia los pokemones por numero al array
-  }
-  return pokemonNum;
 }
 
 function orderList() {
@@ -88,7 +59,7 @@ function orderList() {
       break;
 
     case "ZA": //toma el valor de la opcion seleccionada HTML value = ZA
-      resultOrder = orderZA();
+      resultOrder = orderAZ().reverse();
       break;
 
     case "lowest": //toma el valor de la opcion seleccionada HTML value = lowest
@@ -96,123 +67,11 @@ function orderList() {
       break;
 
     case "higher": //toma el valor de la opcion seleccionada HTML value = higher
-      resultOrder = orderHighest();
+      resultOrder = orderLowest().reverse();
       break;
   }
 
   return resultOrder;
-}
-
-function orderAZ() {
-  // Ordenado A-Z
-  let pokemonNameAZ = buildPokemonNames().sort(); //ordena los pokenones de la A-Z
-  let pokemonAz = new Array(); // guarda la ejecucion de la funcion
-
-  for (const name of pokemonNameAZ) {
-    // busca en la lista de nombres ordenados por nombre
-    for (const pokemon of pokemonList) {
-      // busca el arreglo de objetos del pokemon en la lista de todos los pokemones
-      if (pokemon.name === name) {
-        // compara los atributos por nombres
-        pokemonAz.push(pokemon); //envia los pokemones por numero al array
-      }
-    }
-  }
-  return pokemonAz;
-}
-
-function orderZA() {
-  //ordena los pokemones de la Z-A
-  let pokemonNameZA = buildPokemonNames().sort().reverse(); //ordena los nombres de la A-Z
-  let pokemonZa = new Array(); // guarda la ejecucion de la funcion
-  console.log(pokemonNameZA);
-  for (const name of pokemonNameZA) {
-    // busca por nombre en la lista de nombres
-    for (const pokemon of pokemonList) {
-      // busca el arreglo de objetos del pokemon en la lista de todos los pokemones
-      if (pokemon.name === name) {
-        // compara el pokemon por nombre
-        pokemonZa.push(pokemon); // envia el pokemon por nombre ordenado a la lista de pokemones AZ
-      }
-    }
-  }
-  return pokemonZa;
-}
-
-function orderLowest() {
-  //Ordena los pokemon por número MENOR
-  let pokemonNumLowest = buildPokemonNums().sort(function (a, b) {
-    return a - b;
-  }); //Ordena los pokemon por número 001-251 comparando el valor de su atributo num
-  let pokemonLowest = new Array(); // guarda la ejecucion de la funcion
-
-  for (const num of pokemonNumLowest) {
-    // busca por el numero del pokemon en la lista de pokemones ordenados
-    for (const pokemon of pokemonList) {
-      // busca el arreglo de objetos del pokemon en la lista de todos los pokemones
-      if (pokemon.num === num) {
-        // compara el pokemon por su numero
-        pokemonLowest.push(pokemon); // envia el pokemon por nombre ordenado a la lista de pokemones LOWEST
-      }
-    }
-  }
-  return pokemonLowest;
-}
-
-function orderHighest() {
-  //Ordena los pokemon por número MAYOR
-  let pokemonNumHigher = buildPokemonNums().sort(function (a, b) {
-    return b - a;
-  }); //Ordena los pokemon por número 251-001 comparando el valor de su atributo num
-  let pokemonHigher = new Array(); // guarda la ejecucion de la funcion
-  console.log(pokemonNumHigher);
-
-  for (const num of pokemonNumHigher) {
-    // busca por el numero del pokemon en la lista de pokemones ordenados
-    for (const pokemon of pokemonList) {
-      // busca el arreglo de objetos del pokemon en la lista de todos los pokemones
-      if (pokemon.num === num) {
-        // compara el pokemon por su numero
-        pokemonHigher.push(pokemon); // envia el pokemon por nombre ordenado a la lista de pokemones HIGHER
-      }
-    }
-  }
-  return pokemonHigher;
-}
-
-///////////////////////FILTRADO
-
-function fillterType() {
-  let FilTypeSelected = document.getElementById("filter").value; //toma el valor de la opcion seleccionada HTML id=filter
-  let FilType = new Array(); // guarda la ejecucion del for
-
-  for (const pokemon of pokemonList) {
-    // busca plos pokemones en la lista de pokemones
-    if (
-      pokemon.type[0] === FilTypeSelected ||
-      pokemon.type[1] === FilTypeSelected
-    ) {
-      // busca el valor en la posición 1 o 2 de los tipos
-      FilType.push(pokemon); // envia el pokemon filtrado al array
-    }
-  }
-  return FilType;
-}
-
-/////////////////////// BUSQUEDA
-
-function inputPokemon() {
-  let inputPokemonSearch = document
-    .getElementById("inputSearch")
-    .value.toLowerCase();
-  let pokemonSelected = new Array();
-
-  for (const pokemon of pokemonList) {
-    if (pokemon.name.includes(inputPokemonSearch)) {
-      pokemonSelected.push(pokemon);
-    }
-  }
-  return pokemonSelected;
 }
 
 document.getElementById("botonSearch").addEventListener(
@@ -243,3 +102,42 @@ document.getElementById("filter").addEventListener(
 );
 
 buildCards(orderLowest()); // callback funcion lowest para mostrar en pag iniciada
+/*
+function pokemonShowInfo () {
+  
+    const pokemonInfo = document.getElementById('pokemon-info');
+    let pokemonNum = document.createElement("div");
+    pokemonNum.className = "property num";
+
+    let pokemonName = document.createElement("div");
+    pokemonName.className = "property name";
+
+    let pokemonImg = document.createElement("img");
+    pokemonImg.setAttribute("src", `${pokemonList.img}`);
+    pokemonImg.className = "property img";
+
+    let barrita = document.createElement("div");
+    barrita.className = "barrita";
+
+    let num = document.createTextNode(`${pokemonList.num}`);
+    let name = document.createTextNode(`${pokemonList.name}`);
+
+    pokemonNum.appendChild(num);
+    pokemonName.appendChild(name);
+    pokemonInfo.appendChild(pokemonNum);
+    pokemonInfo.appendChild(pokemonImg);
+    pokemonInfo.appendChild(pokemonName);
+    pokemonInfo.appendChild(barrita);
+
+    for (let type of pokemonList.type) {
+      let divType = document.createElement("div"); // separa los tipos en divs
+      divType.id = "divType"; //agrega clase a los divs
+      divType.className = type; //agrega clase background por tipo
+      let textType = document.createTextNode(type); //crea el texto del tipo
+      divType.appendChild(textType);
+      pokemonInfo.appendChild(divType);
+  }
+}
+
+const buttonCards = document.querySelectorAll('.card');
+buttonCards.addEventListener('click', pokemonShowInfo, true);*/
