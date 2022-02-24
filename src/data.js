10734fae3da43ec1a5d7cb37cf683798e1a714ae
veroguzrob72 <vgrdesd@gmail.com>
2022-02-24 12:33:24 -0600

    Filtro por roles funcionando

--- src/data.js
+++ src/data.js
@@ -1,16 +1,15 @@
-import legends from './lol/lol.js';
-
-export const all = Object.values(legends.data)
-//export const sortAz = all.sort((a, b) => (a.id > b.id ? -1 : 1));
-//export const sortZa = all.sort((a, b) => (a.id < b.id ? 1 : -1));
+/*export const sortAz = all.sort((a, b) => (a.id > b.id ? -1 : 1));
+export const sortZa = all.sort((a, b) => (a.id < b.id ? 1 : -1));
 export const filterMage = all.filter(tags => tags.tags.includes('Mage'));
 export const filterAssassin = all.filter(tags => tags.tags.includes('Assassin'));
 export const filterFighter = all.filter(tags => tags.tags.includes('Fighter'));
 export const filterTank = all.filter(tags => tags.tags.includes('Tank'));
 export const filterSupport = all.filter(tags => tags.tags.includes('Support'));
-export const filterMarksman = all.filter(tags => tags.tags.includes('Marksman'))
+export const filterMarksman = all.filter(tags => tags.tags.includes('Marksman'))*/
 
-
-//console.log (filterTank);
-
+export const filterRole = (champions, roles) =>{
+   const filterChampions = champions.filter(tags => tags.tags.includes(roles));
+   console.log(filterChampions)
+   //return filterRole
+}
 
