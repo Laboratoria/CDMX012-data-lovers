9bdd7ade08e01817fff2cacc57d8e2dc31250050
roxanaggr <roxanaggr@gmail.com>
2022-02-27 12:45:39 -0600

    renderizado de campeones haciendo clic en filtro para desktop y movil

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import {filterRole} from './data.js';
 import legends from './lol/lol.js';
+
 const all = Object.values(legends.data)
 
 const cardsLegends = () =>{
@@ -19,10 +20,51 @@ const cardsLegends = () =>{
  } 
  return championsCards
 }
-document.getElementById('cardsChampions').appendChild(cardsLegends())
 
+document.getElementById('cardsChampions').appendChild(cardsLegends())
+//una variable para llamar el innerHTML vacío
+const container = document.getElementById('cardsChampions')
+
+//selecciona el filtro al hacer click en la opción del dropdown
 const rolChampion = document.querySelector('.role');
   rolChampion.addEventListener('change', (e)=>{
-    filterRole(all, e.target.value)
-  })
+    //se crea una variable de todos los filtros para poder guardar los resultados y jalarlos después
+    let filteredRoles = filterRole(all, e.target.value)
+    //un innerHTML vacío para construir las tarjetas iterando en los filtros
+    container.innerHTML = '';
+    let cardsRoles = () => {
+      let championsCards = document.createElement('div')
+      for (let role of filteredRoles){
+      let newCard = document.createElement('article');
+      let newcardBody = document.createElement('figure');
+      let newcardImage = document.createElement('img');
+      newcardImage.src = role.splash
+      let newcardName = document.createElement('figcaption');
+      newcardName.textContent = role.name
+      newcardName.className = "card-name";
+      championsCards.appendChild(newCard);
+      newCard.appendChild(newcardBody);
+      newcardBody.appendChild(newcardImage);
+      newcardBody.appendChild(newcardName);  
+    }
+    return championsCards
+ } 
+ //en este elemento se renderizan los campeones
+ document.getElementById('cardsChampions').appendChild(cardsRoles())
+});
+
+
+//Botón Toggle Responsive
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navLinks = document.getElementsByTagName('nav')[0]
+const titleHeader = document.getElementsByClassName('htype')[0]
+//Haz una funcion que me tome todos los links y los muestre
+toggleButton.addEventListener('click', () => {
+  //si no existe la clase active, la agregará y si existe la quita
+  navLinks.classList.toggle('active')
+  titleHeader.classList.toggle('active')
+});
+
+
+
 
