894e6f85660f6010151f49e5a15a7f37424ca56e
AnaCif98 <inv0320@innovaccion.mx>
2022-03-01 12:25:39 -0700

    Agregué función sort

--- src/main.js
+++ src/main.js
@@ -1,6 +1,38 @@
-import { example } from './data.js';
+import { ordenar } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+import data from './data/rickandmorty/rickandmorty.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+const valores = data.results; //esta tomando los datos de Rick y Morty, pero únicamente 
+const dataPorNombre = ordenar(valores);
+const valorInput = document.getElementById("buscar");
+const botonLupa = document.getElementById("lupa");
+const posicion = document.getElementById("all"); 
+
+
+botonLupa.addEventListener("click", buscador);
+function buscador()// Al dar click a la lupa, 
+        {                            
+          let buscar = valorInput.value.toLowerCase();
+          let contador = [];
+          posicion.innerHTML = ""; 
+             if(valorInput.value != ""){    
+                for (let valor of dataPorNombre){
+                  let nombre = valor.name.toLowerCase();  
+                    if(nombre.indexOf(buscar) !== -1){
+                      contador.push(valor.name);
+                      posicion.innerHTML +=  `<div class= "conte" >
+                      <div class = "frente"> 
+                      <img src="rickandmorty/pasaporte.png" alt="Pasaporte" class="pasaporte" > 
+                      <p id="nombre" class="nombres" >${valor.name}</p>
+                      </div>
+                      <div class= "atras">
+                      <a href="#${valor.id}"><img src="${valor.image}" alt="Pasaporte" class="foto"></a>
+                      <p class="nombres">${valor.name}</p>
+                      </div>
+                      </div>
+                    `   
+                                     }
+               }  }
+               console.log(contador);
+                        }
