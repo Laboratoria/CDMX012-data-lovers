73a3d173ebaa1444c68e1b2701c938d7e19cb6aa
nathaliesainz <nathaliesainz13@gmail.com>
2022-03-01 21:28:56 -0600

    Data Lovers actualizado, función de ordenado, visualización de películas en pantalla y diseños

--- src/data.js
+++ src/data.js
@@ -1,9 +1,32 @@
-// estas funciones son de ejemplo
+/////mostrar cards en pantalla////
+export function filmsOnScreen(films){
 
-export const films = () => {
-  return films;
-};
+  return `<div class="cardFilms">
+  <p><img src="${films.poster}"</p>
+  <h2>${films.title}</h2>
+  <h3>${films.release_date}</h3>
+  </div>`;
+}
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+////ordenado de cards A-Z Y Z-A////
+export function sortAZ(option, dataFilms){
+  return dataFilms.sort(function (a, b) {
+     if (option=='az'){
+        if (a.title > b.title) {
+          return 1;
+        }
+        if (a.title < b.title) {
+          return -1;
+        }
+        return 0;
+     }else if(option=='za'){
+       if (a.title > b.title) {
+         return -1;
+       }
+       if(a.title < b.title){
+         return 1;
+       }
+       return 1;
+     }
+    });
+}
--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
-import { films } from './data.js';
 import data from './data/ghibli/ghibli.js';
+import {filmsOnScreen} from './data.js';
+import { sortAZ } from './data.js'
 
-console.log(data, films);
+/////mostrar cards en pantalla////
+const nodoRoot = document.getElementById("root")
+let insertHTML = ""
+let btnAZ = document.getElementById("sortFilms")
+
+data.films.forEach(film  => {
+  insertHTML += filmsOnScreen(film)
+})
+
+nodoRoot.innerHTML=insertHTML
+
+////ordenado de cards////
+btnAZ.addEventListener('change', (e) => {
+  let sortedFilms=sortAZ(e.target.value, data.films)
+  let htmlOrdenado=""
+  sortedFilms.forEach(oneTitle =>{
+    htmlOrdenado+=filmsOnScreen(oneTitle)
+  })
+  nodoRoot.innerHTML=htmlOrdenado;
+})
