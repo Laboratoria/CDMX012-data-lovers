86b5fb51970c28bdb914c60cc59dbd4b3dd4ca0c
nathaliesainz <nathaliesainz13@gmail.com>
2022-08-04 18:52:43 -0500

    Removed styles, added new ones and changed background

--- src/data.js
+++ src/data.js
@@ -1,14 +1,13 @@
-/////mostrar cards en pantalla////
 export function filmsOnScreen(films){
-
+///función para mostrar datos en cards///
   return `<div class="cardFilms">
-  <p><img src="${films.poster}"</p>
+  <p><img src="${films.poster}" class="posterFilm"</p>
   <h2>${films.title}</h2>
   <h3>${films.release_date}</h3>
   </div>`;
 }
 
-////ordenado de cards A-Z Y Z-A////
+///función para ordenar cards A-Z y Z-A///
 export function sortAZ(option, dataFilms){
   return dataFilms.sort(function (a, b) {
      if (option=='az'){
@@ -26,7 +25,30 @@ export function sortAZ(option, dataFilms){
        if(a.title < b.title){
          return 1;
        }
-       return 1;
+       return 0;
+     }
+    });
+}
+
+///función para ordenar cards cronológicamente///
+export function sortYears(option, dataFilms){
+  return dataFilms.sort(function (a, b) {
+     if (option=='newest'){
+        if (a.release_date < b.release_date) {
+          return 1;
+        }
+        if (a.release_date > b.release_date) {
+          return -1;
+        }
+        return 0;
+     }else if(option=='oldest'){
+       if (a.release_date < b.release_date) {
+         return -1;
+       }
+       if(a.release_date > b.release_date){
+         return 1;
+       }
+       return 0;
      }
     });
 }
--- src/main.js
+++ src/main.js
@@ -1,24 +1,34 @@
 import data from './data/ghibli/ghibli.js';
-import {filmsOnScreen} from './data.js';
-import { sortAZ } from './data.js'
+import { filmsOnScreen, sortAZ, sortYears } from './data.js';
 
-/////mostrar cards en pantalla////
+///declaración de variables///
 const nodoRoot = document.getElementById("root")
 let insertHTML = ""
 let btnAZ = document.getElementById("sortFilms")
 
+///mostrar cards en pantalla///
 data.films.forEach(film  => {
   insertHTML += filmsOnScreen(film)
 })
 
 nodoRoot.innerHTML=insertHTML
 
-////ordenado de cards////
+///ordenar de cards de la AZ-ZA///
 btnAZ.addEventListener('change', (e) => {
   let sortedFilms=sortAZ(e.target.value, data.films)
   let htmlOrdenado=""
   sortedFilms.forEach(oneTitle =>{
     htmlOrdenado+=filmsOnScreen(oneTitle)
+  })
+  nodoRoot.innerHTML=htmlOrdenado;
+})
+
+///orden en orden cronológico///
+btnAZ.addEventListener('change', (e) => {
+  let sortedYears=sortYears(e.target.value, data.films)
+  let htmlOrdenado=""
+  sortedYears.forEach(oneYear =>{
+    htmlOrdenado+=filmsOnScreen(oneYear)
   })
   nodoRoot.innerHTML=htmlOrdenado;
 })
